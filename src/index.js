02646aec86efbba5195b6cd764233d1d7d7fe5c3
Noah Leigh <noah.d.leigh@gmail.com>
2018-06-28 17:01:21 -0400

    drawBigCenteredTest takes an array of strings, styling altered, draws white box behind text

--- src/index.js
+++ src/index.js
@@ -53,21 +53,80 @@ const fillCanvasWithImage = (ctx, image) => {
 }
 
 /**
- * Draws the text in large white lettering with black strokes in the center of the canvas
- * @param {CanvasRenderingContext2D} ctx - The canvas context
- * @param {String} text - The text to go on the canvas
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * Source: https://stackoverflow.com/a/3368118/9165387
+ * @param {CanvasRenderingContext2D} ctx The canvas context
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} [radius = 5] The corner radius; It can also be an object
+ *                 to specify different radii for corners
+ * @param {Number} [radius.tl = 0] Top left
+ * @param {Number} [radius.tr = 0] Top right
+ * @param {Number} [radius.br = 0] Bottom right
+ * @param {Number} [radius.bl = 0] Bottom left
+ * @param {Boolean} [fill = false] Whether to fill the rectangle.
+ * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
  */
-const drawBigCenteredText = (ctx, text) => {
+const roundRect = (ctx, x, y, width, height, radius, fill, stroke) => {
+  if (typeof stroke === 'undefined') {
+    stroke = true
+  }
+  if (typeof radius === 'undefined') {
+    radius = 5
+  }
+  if (typeof radius === 'number') {
+    radius = {tl: radius, tr: radius, br: radius, bl: radius}
+  } else {
+    var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0}
+    for (var side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side]
+    }
+  }
+  ctx.beginPath()
+  ctx.moveTo(x + radius.tl, y)
+  ctx.lineTo(x + width - radius.tr, y)
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr)
+  ctx.lineTo(x + width, y + height - radius.br)
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height)
+  ctx.lineTo(x + radius.bl, y + height)
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl)
+  ctx.lineTo(x, y + radius.tl)
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y)
+  ctx.closePath()
+  if (fill) {
+    ctx.fill()
+  }
+  if (stroke) {
+    ctx.stroke()
+  }
+}
+
+/**
+ * Draws the text in large black lettering over a white rounded rect in the center of the canvas
+ * @param {CanvasRenderingContext2D} ctx - The canvas context
+ * @param {String[]} textArray - Array of strings to go on the canvas. Each string will be on a line.
+ */
+const drawBigCenteredText = (ctx, textArray) => {
   ctx.save()
   ctx.fillStyle = 'white'
   ctx.strokeStyle = 'black'
-  // ctx.shadowColor = 'black'
-  // ctx.shadowBlur = 2
-  ctx.font = 'bold 32px sans-serif'
+  ctx.lineWidth = 2
+  const fontSize = 48
+  ctx.font = `bold ${fontSize}px sans-serif`
   ctx.textAlign = 'center'
   ctx.textBaseline = 'middle'
-  ctx.fillText(text, ctx.canvas.width / 2, ctx.canvas.height / 2, ctx.canvas.width)
-  ctx.strokeText(text, ctx.canvas.width / 2, ctx.canvas.height / 2, ctx.canvas.width)
+  const textStartingY = (ctx.canvas.height - ((textArray.length - 1) * fontSize)) / 2
+  roundRect(ctx, 5, textStartingY - fontSize / 2, ctx.canvas.width - 10, textArray.length * fontSize, 10, true)
+  for (let line = 0; line < textArray.length; line += 1) {
+    ctx.fillStyle = 'black'
+    const textY = textStartingY + (line * fontSize)
+    ctx.fillText(textArray[line], ctx.canvas.width / 2, textY, ctx.canvas.width - 10)
+    // ctx.strokeText(textArray[line], ctx.canvas.width / 2, textY, ctx.canvas.width)
+  }
   ctx.restore()
 }
 
