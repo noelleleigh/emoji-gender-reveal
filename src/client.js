0b04391e256b66a2dcca729815781bc403048b65
Noelle Leigh <noelle_leigh@fastmail.com>
2019-05-29 17:07:51 -0400

    Added emoji query param to client page

--- src/client.js
+++ src/client.js
@@ -7,6 +7,12 @@ const canvas = document.getElementById('canvas')
 canvas.width = 540
 canvas.height = 480
 const ctx = canvas.getContext('2d')
-canvas.addEventListener('click', newEmoji(ctx, filteredEmojiArray))
+const preselectedEmoji = (new URL(document.location)).searchParams.get('emoji')
+if (preselectedEmoji) {
+  const singleEmojiArray = filteredEmojiArray.filter(emojiObj => emojiObj.char === preselectedEmoji)
+  canvas.addEventListener('click', newEmoji(ctx, singleEmojiArray))
+} else {
+  canvas.addEventListener('click', newEmoji(ctx, filteredEmojiArray))
+}
 
 drawTitleScreen(ctx)
