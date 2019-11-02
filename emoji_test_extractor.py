23f5cde8356b9a0302b6089f1a14d3d1689da260
Noelle Leigh <noelle_leigh@fastmail.com>
2019-11-02 16:38:38 -0400

    Support unqualified emoji in emoji.json
    Instead of a list, this is an object keyed to an emoji string with a
    value that references an object that contains the fully-qualified
    version of the emoji.

--- emoji_test_extractor.py
+++ emoji_test_extractor.py
@@ -9,7 +9,11 @@ import urllib.request
 
 # Matches the fully-qualified entries in the file
 # and has named groups for the character and the description of the emoji
-REGEX = re.compile(r'^[^#]*; fully-qualified\s+# (?P<char>[^ ]+) E\d+\.\d (?P<descr>.+)$')  # noqa: E501
+REGEX = re.compile(r'^[^#]*; (?P<status>[^ ]+)\s+# (?P<char>[^ ]+) E\d+\.\d (?P<descr>.+)$')  # noqa: E501
+
+FULLY_QUALIFIED = "fully-qualified"
+UNQUALIFIED = "unqualified"
+ALLOWED_STATUSES = set((FULLY_QUALIFIED, UNQUALIFIED))
 
 
 def test_line(string):
@@ -29,15 +33,43 @@ def extract_info(string):
 
 
 def main(url):
-    """Download the url and extract its information into a list of dicts."""
+    """Download the url and extract its information into a dictionary.
+
+    Unqualified emoji will reference their fully-qualified versions.
+    """
+    emoji_map = {}
+    qualified_match = None
     with urllib.request.urlopen(url) as response:
         body = response.read().decode()
-        emoji = [
-            extract_info(line) for line in
-            body.split('\n')
-            if test_line(line)
-        ]
-        return emoji
+        lines = body.split('\n')
+
+        for line in lines:
+            if not test_line(line):
+                # Not an emoji line
+                continue
+
+            match = re.match(REGEX, line)
+            if match.group('status') not in ALLOWED_STATUSES:
+                # Not one of the two statuses
+                continue
+
+            if match.group('status') == FULLY_QUALIFIED:
+                emoji_map[match.group('char')] = {
+                    'char': match.group('char'),
+                    'descr': match.group('descr')
+                }
+                # Save the match if the next line is unqualified
+                qualified_match = match
+            elif match.group('status') == UNQUALIFIED:
+                emoji_map[match.group('char')] = {
+                    'char': qualified_match.group('char'),
+                    'descr': qualified_match.group('descr')
+                }
+            else:
+                # How did you get here?
+                raise ValueError(f'Unknown status: {match.group("status")}')
+
+        return emoji_map
 
 
 if __name__ == '__main__':
